1b13cf9c1fbfa9dc16d9a942ceb8e4c2932c7f25
allysonkaye <allysonkayetimpug06@gmail.com>
2024-03-08 22:24:53 +0800

    add task

--- app/src/main/java/com/example/todolist/MainActivity.java
+++ app/src/main/java/com/example/todolist/MainActivity.java
@@ -1,18 +1,29 @@
 package com.example.todolist;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.View;
 
+import com.example.todolist.Model.todoModel;
 import com.example.todolist.Util.databaseHelper;
+import com.example.todolist.adapter.todoAdapter;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity implements onDialogCloseListener {
     private RecyclerView recyclerView;
     private FloatingActionButton fab;
     private databaseHelper db;
+    private List<todoModel> list;
+    private todoAdapter adapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,12 +32,30 @@ public class MainActivity extends AppCompatActivity {
         recyclerView = findViewById(R.id.todolist);
         fab = findViewById(R.id.floatingActionButton);
         db = new databaseHelper(MainActivity.this);
+        list = new ArrayList<>();
+        adapter = new todoAdapter(db , MainActivity.this);
+
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setAdapter(adapter);
+
+        list = db.getAllTasks();
+        Collections.reverse(list);
+        adapter.setTask(list);
 
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                addTask.newInstances().show(getSupportFragmentManager() , addTask.TAG);
             }
         });
+    }
+
+    @Override
+    public void onDialogClose(DialogInterface dialogInterface) {
+        list = db.getAllTasks();
+        Collections.reverse(list);
+        adapter.setTask(list);
+        adapter.notifyDataSetChanged();
     }
 }
--- app/src/main/java/com/example/todolist/addTask.java
+++ app/src/main/java/com/example/todolist/addTask.java
@@ -1,5 +1,8 @@
 package com.example.todolist;
 
+import android.app.Activity;
+import android.content.DialogInterface;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
@@ -12,20 +15,25 @@ import android.widget.EditText;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 
+import com.example.todolist.Model.todoModel;
 import com.example.todolist.Util.databaseHelper;
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment;
 
 public class addTask extends BottomSheetDialogFragment {
-    private static final String TAG = "AdddNewTask";
+    public static final String TAG = "addTask";
 
     private EditText add_txt;
     private Button btn_add;
     private databaseHelper db;
 
+    public static addTask newInstances() {
+        return new addTask();
+    }
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View v = inflater(R.layout.add_task , container , false);
+        View v = inflater.inflate(R.layout.add_task , container , false);
         return v;
 
 
@@ -41,12 +49,12 @@ public class addTask extends BottomSheetDialogFragment {
         db = new databaseHelper(getActivity());
 
         boolean isUpdate = false;
+
         Bundle bundle = getArguments();
         if (bundle != null) {
             isUpdate = true;
             String task = bundle.getString("task");
-            add_txt.getText(task);
-
+            add_txt.setText(task);
             if (task.length() > 0){
                 btn_add.setEnabled(false);
             }
@@ -60,6 +68,13 @@ public class addTask extends BottomSheetDialogFragment {
             @Override
             public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
 
+                if ( charSequence.toString().equals("")) {
+                    btn_add.setEnabled(false);
+                    btn_add.setBackgroundColor(Color.GRAY);
+                } else {
+                    btn_add.setEnabled(true);
+                    btn_add.setBackgroundColor(getResources().getColor(R.color.violet));
+                }
             }
 
             @Override
@@ -67,5 +82,33 @@ public class addTask extends BottomSheetDialogFragment {
 
             }
         });
+
+        final boolean finalIsUpdate = isUpdate;
+        btn_add.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String text = add_txt.getText().toString();
+
+                if(finalIsUpdate) {
+                    db.updateTask(bundle.getInt("id") , text);
+
+                }else {
+                    todoModel item = new todoModel();
+                    item.setTask(text);
+                    item.setStatus(0);
+                    db.insertTask(item);
+                }
+                dismiss();
+            }
+        });
+    }
+
+    @Override
+    public void onDismiss(@NonNull DialogInterface dialog) {
+        super.onDismiss(dialog);
+        Activity activity = getActivity();
+        if (activity instanceof onDialogCloseListener) {
+            ((onDialogCloseListener) activity).onDialogClose(dialog);
+        }
     }
 }
